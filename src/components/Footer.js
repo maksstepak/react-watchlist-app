3cb45bd61b685fab5ad8afc8c5708880c7e2c262
maksstepak <maks.stepak1@gmail.com>
2020-12-21 19:54:21 +0100

    Add header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>Copyright &copy; 2020 Foo Bar</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="hero">
+        <div className="hero-body">
+          <h1 className="title">Watchlist App</h1>
+          <h2 className="subtitle">You have X movies to watch</h2>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
