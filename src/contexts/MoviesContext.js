07139bdd9e275d3aa346b09a1a734aab7577dd98
maksstepak <maks.stepak1@gmail.com>
2020-12-24 15:59:19 +0100

    Add local storage sync

--- src/contexts/MoviesContext.js
+++ src/contexts/MoviesContext.js
@@ -1,12 +1,20 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
 import { reducer } from '../reducers/moviesReducer';
 
 export const MoviesContext = createContext();
 
 const initialState = [];
 
+const initializer = () => {
+  return JSON.parse(localStorage.getItem('movies')) || [];
+};
+
 export const MoviesContextProvider = ({ children }) => {
-  const [movies, dispatch] = useReducer(reducer, initialState);
+  const [movies, dispatch] = useReducer(reducer, initialState, initializer);
+
+  useEffect(() => {
+    localStorage.setItem('movies', JSON.stringify(movies));
+  }, [movies]);
 
   return (
     <MoviesContext.Provider value={{ movies, dispatch }}>
